f5244697607d5ca2f672babe53e97caabd8206ca
krc14b <krc14b@my.fsu.edu>
2017-11-23 01:21:15 -0500

    Updated test classes with direction for where to go when tests are ready to be made

--- tests/pollTests.py
+++ tests/pollTests.py
@@ -6,21 +6,39 @@ class TestUserMethods(unittest.TestCase):
     def testPollCreate(self):
         self.poll = Poll.create_poll()
         print ("Created Poll")
+        """
+        Once create_poll is made we will assert that self.poll
+        has all the desired properties here
+        """
 
     def testPollUpdate(self):
         self.poll = Poll.create_poll()
+        # Update poll with new properties here
         Poll.update_poll(self.poll)
         print ("Updated Poll")
+        """
+        Once update_poll is made we will assert that self.poll's 
+        properties fit the new properties given
+        """
 
     def testPollDelete(self):
         self.poll = Poll.create_poll()
         Poll.delete_poll(self.poll)
         print ("Deleted Poll")
+        """
+        Once delete_poll is made we will assert that self.poll
+        no longer exists or is equivalent to null
+        """
 
     def testPollView(self):
         self.poll = Poll.create_poll()
         Poll.view_poll(self.poll)
         print ("Poll is viewable")
+        """
+        Once view_poll is made we will assert that self.poll is
+        viewable. That or this will just have to be a test done
+        manually because I cant see a way to automate this *yet*
+        """
 
 
 if __name__ == '__main__':
--- tests/userTests.py
+++ tests/userTests.py
@@ -6,17 +6,29 @@ class TestUserMethods(unittest.TestCase):
     def testUserCreate(self):
         self.user = User.create_user()
         print ("Created user")
+        """
+        Once create_user is made we will assert that self.user 
+        has all the desired properties here
+        """
 
     def testUserUpdate(self):
         self.user = User.create_user()
+        # Update user with new properties here
         User.update_user(self.user)
         print ("Updated user")
+        """
+        Once update_user is made we will assert that self.user's 
+        properties fit the new properties given
+        """
 
     def testUserDelete(self):
         self.user = User.create_user()
         User.delete_user(self.user)
         print ("Deleted user")
-
+        """
+        Once delete_user is made we will assert that self.user
+        no longer exists or is null
+        """
 
 if __name__ == '__main__':
     unittest.main()
