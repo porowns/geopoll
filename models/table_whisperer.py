68cc26d75caa4c84a5a5fbe8cb7e9f70c4006e50
Nicholas <kelton.nicholas@gmail.com>
2017-11-24 13:03:40 -0500

    1) added sign-in page 2) added query function to facilitate user sign in

--- models/table_whisperer.py
+++ models/table_whisperer.py
@@ -17,5 +17,25 @@ def insert_new_user(name, email, pword):
     #instances = session.query(User).all()
     #for i in instances:
     #    print(i.user_name,i.user_email,i.user_pword)
-
     session.close()
+
+
+def account_sign_in(accName, pword):
+    user_db_session = sessionmaker(bind=engine)
+    session = user_db_session()
+    q = None
+    if '@' in accName:
+        email = accName
+        q = session.query(User).filter_by(user_email=email).filter_by(user_pword=pword).first()
+    else:
+        name = accName
+        q = session.query(User).filter_by(user_name=name).filter_by(user_pword=pword).first()
+
+    if q is not None:
+        session.close()
+        return True
+    else:
+        session.close()
+        return False
+
+
--- views.py
+++ views.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, json, request
 
-from models.insert_new_user import insert_new_user
+from models.table_whisperer import insert_new_user, account_sign_in
 
 app = Flask(__name__)
 
@@ -14,6 +14,7 @@ def main():
 def showSignUp():
     return render_template('signup.html')
 
+
 @app.route('/signUp', methods=['POST'])
 def signUp():
     # read the posted values
@@ -24,9 +25,27 @@ def signUp():
     # validate values
     if _name and _email and _pword:
         insert_new_user(_name, _email, _pword)
-        return json.dumps({'html':'<span>All fields good!</span>'})
+        return json.dumps({'html': '<span>All fields good!</span>'})
     else:
         return json.dumps({'html':'<span>Enter the required fields</span>'})
+
+
+@app.route('/showSignIn')
+def showSignIn():
+    return render_template('signin.html')
+
+
+@app.route('/signIn', methods=['POST','GET'])
+def signIn():
+    _acc_name = request.form['inputAccName']
+    _pword = request.form['inputPassword']
+
+    if _acc_name and _pword:
+        account_sign_in(_acc_name, _pword)
+        return json.dumps({'html':'<span>All fields good!</span>'})
+    else:
+        return json.dumps({'html': '<span>Enter the required fields</span>'})
+
 
 if __name__ == '__main__':
     app.run()
