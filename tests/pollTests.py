529706d3c95f8cd9afaedcbec9e101ab617d4f81
krc14b <krc14b@my.fsu.edu>
2017-11-24 23:19:57 -0500

    Updated pollTests.py and userTests.py with more in-depth testing Added placeholders for possible future tests in pollTests.py and userTests.py

--- tests/pollTests.py
+++ tests/pollTests.py
@@ -14,12 +14,26 @@ class TestUserMethods(unittest.TestCase):
         has all the desired properties here
         """
 
+    def testPollQuestionCreate(self):
+        poll = Poll('Test', User(user_name='name', user_email='email', user_pword='pword'))
+        questionType = 'Multiple Choice'
+        quesitonText = 'Is this a question?'
+        pollID = poll.poll_id
+        insert_new_question(question_type=questionType, question_text=quesitonText, poll_id=pollID)
+        print("New Question Created")
+
     def testPollUpdate(self):
         print ("Updated Poll")
         """
         Once update_poll is made we will assert that self.poll's 
         properties fit the new properties given
         """
+
+    def testPollQuestionUpdate(self):
+        """
+        A test to test updating a question already created
+        """
+        pass
 
     def testPollDelete(self):
         print ("Deleted Poll")
@@ -28,6 +42,12 @@ class TestUserMethods(unittest.TestCase):
         no longer exists or is equivalent to null
         """
 
+    def testPollQuestionDelete(self):
+        """
+        A test to test deleting a question already created
+        """
+        pass
+
     def testPollView(self):
         print ("Poll is viewable")
         """
@@ -35,6 +55,18 @@ class TestUserMethods(unittest.TestCase):
         viewable. That or this will just have to be a test done
         manually because I cant see a way to automate this *yet*
         """
+
+    def testPollAddQuestion(self):
+        """
+        A test to test adding a question to a poll
+        """
+        pass
+
+    def testPollResponse(self):
+        """
+        A test to test poll responses however we choose to do that
+        """
+        pass
 
 
 if __name__ == '__main__':
--- tests/userTests.py
+++ tests/userTests.py
@@ -20,21 +20,31 @@ class TestUserMethods(unittest.TestCase):
         email = 'email@email.com'
         password = 'password'
         insert_new_user(name, email, password)
+
         # Query by name
         nameQuery = user_query(name, 1)
         self.assertTrue(nameQuery.user_name == name)
         self.assertTrue(nameQuery.user_email == email)
         self.assertTrue(nameQuery.user_pword == password)
 
+        #Query by email
         emailQuery = user_query(email, 2)
         self.assertTrue(emailQuery.user_name == name)
         self.assertTrue(emailQuery.user_email == email)
         self.assertTrue(emailQuery.user_pword == password)
 
+        # Query by password
         pwordQuery = user_query(password, 3)
         self.assertTrue(pwordQuery.user_name == name)
         self.assertTrue(pwordQuery.user_email == email)
         self.assertTrue(pwordQuery.user_pword == password)
+
+        # Query by id
+        id = nameQuery.user_id
+        idQuery = user_query(id, 0)
+        self.assertTrue(idQuery.user_name == name)
+        self.assertTrue(idQuery.user_email == email)
+        self.assertTrue(idQuery.user_pword == password)
 
     def testUserUpdate(self):
         print ("Updated user")
